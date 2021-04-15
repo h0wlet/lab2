af8db7bbc15d4fc2ffed6b0dcdfaff800af41f28
h0wlet <71941088+h0wlet@users.noreply.github.com>
2021-04-15 23:33:09 +0700

    Update ReadFile.java

--- src/command/ReadFile.java
+++ src/command/ReadFile.java
@@ -5,6 +5,8 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import Exception.*;
+
 public class ReadFile implements Command {
     static Logger logger = Logger.getLogger(Sort.class.getName());
 
@@ -14,8 +16,8 @@ public class ReadFile implements Command {
         logger.info("Execution began!");
 
         if (arguments.size() != 1) {
-            //throw new Exception("Args for read != 1");
             logger.log(Level.WARNING, "Args for read != 1");
+            throw new WrongNumberOfArguments("Args for read != 1");
         }
 
         File file = new File(arguments.get(0));
